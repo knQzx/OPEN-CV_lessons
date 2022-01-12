61365870617c9f2adadbb869c1ffda8616cc3ef3
knqzx <>
2022-01-12 19:54:57 +0300

    already normal recognition of the face and the uniqueness of the face in the photos

--- lesson_3/entrance_to_the_territory.py
+++ lesson_3/entrance_to_the_territory.py
@@ -1,37 +1,64 @@
 import datetime
 import os
 import random
-import time
 import cv2
 import telebot
-from PIL import Image, ImageChops
 
 vfc = cv2.VideoCapture(2)
 
+class Entrance_to_the_territory:
+    def __init__(self):
+        pass
 
-def algorithm_similarities_faces(img: str, directory: str):  # algorithm similarities faces
-    # check differences if there are any
-    image_1 = Image.open(f'{directory}/{img}')
-    # chdir to directory "photos"
-    os.chdir(f'{directory}')
-    # see all photos in the directory "photos"
-    sp_nones = []
-    sp_not_nones = []
-    for images in os.listdir():
-        # open image
-        image_2 = Image.open(images)
+    def algorithm_similarities_faces(self, img: str, directory: str):  # algorithm similarities faces
         # check differences if there are any
-        result = ImageChops.difference(image_1, image_2)
-        # calculates the bounding box of non-zero regions in the image
-        if result.getbbox() is None:
-            sp_nones.append(None)
-        else:
-            sp_not_nones.append(True)
-    # chdir to start directory
-    os.chdir('..')
-    # return send this photo if we didn't have similar photo
-    if len(sp_not_nones) >= len(sp_nones):
-        return 'Send this photo'
+        hash1 = self.calculate_image_hash(f'{directory}/{img}')
+        # chdir to directory "photos"
+        os.chdir(f'{directory}')
+        # see all photos in the directory "photos"
+        for images in os.listdir():
+            # open image
+            hash2 = self.calculate_image_hash(images)
+            # check differences if there are any
+            result = int(self.compare_hash(hash1, hash2))
+            print(result)
+            # calculates the bounding box of non-zero regions in the image
+            if result >= 22:
+                # chdir to start directory
+                os.chdir('..')
+                return 'Send this photo'
+
+
+    def calculate_image_hash(self, FileName: str):  # calculate image hash
+        image = cv2.imread(FileName)  # Let's read the picture
+        resized = cv2.resize(image, (8, 8), interpolation=cv2.INTER_AREA)  # Reduce the picture
+        gray_image = cv2.cvtColor(resized,
+                                  cv2.COLOR_BGR2GRAY)  # We will translate it into black and white format
+        avg = gray_image.mean()  # Average pixel value
+        ret, threshold_image = cv2.threshold(gray_image, avg, 255, 0)  # Binarization by threshold
+
+        # Calculate the hash
+        _hash = ""
+        for x in range(8):
+            for y in range(8):
+                val = threshold_image[x, y]
+                if val == 255:
+                    _hash = _hash + "1"
+                else:
+                    _hash = _hash + "0"
+
+        return _hash
+
+
+    def compare_hash(self, hash1, hash2):  # compare hash
+        l = len(hash1)
+        i = 0
+        count = 0
+        while i < l:
+            if hash1[i] != hash2[i]:
+                count = count + 1
+            i = i + 1
+        return count
 
 
 # load our haar cascade
@@ -51,7 +78,7 @@ while vfc.isOpened():
         # delete files in the folder if there are more than 10 pieces (so as not to clog the memory)
         if 'photos' not in os.getcwd():
             os.chdir('photos')
-        if len(os.listdir()) >= 10:
+        if len(os.listdir()) >= 30:
             for el in os.listdir():
                 os.remove(el)
         if 'photos' in os.getcwd():
@@ -65,7 +92,7 @@ while vfc.isOpened():
         if 'photos' in os.getcwd():
             os.chdir('..')
         if name_file in os.listdir('photos'):
-            result = algorithm_similarities_faces(name_file, 'photos')
+            result = Entrance_to_the_territory().algorithm_similarities_faces(name_file, 'photos')
         # if result "Send this photo", send this photo (logically)
         if result == 'Send this photo':
             # initialization bot
@@ -75,6 +102,5 @@ while vfc.isOpened():
                 os.chdir('..')
             bot.send_photo(763258583, photo=open(f'photos/{name_file}', 'rb'),
                            caption=f'На территорию вошел человек.\nВремя - {datetime.datetime.now()}')
-            time.sleep(2)
     if cv2.waitKey(5) & 0xFF == 27:
         break
